e990a73ecc8e52eecee217f1479708f8e3d35314
Zheng-Ling Lai <jamesljlster@gmail.com>
2018-01-24 16:23:16 +0800

    Add more transfer function declaration and using stable softmax

--- src/cnn_builtin_math.c
+++ src/cnn_builtin_math.c
@@ -1,24 +1,78 @@
 #include <math.h>
 
+#include "cnn.h"
 #include "cnn_builtin_math.h"
+
+CNN_AFUNC_DEF((*cnn_transfer_list[])) = {
+	cnn_softmax,
+	cnn_relu,
+	cnn_swish
+};
+
+CNN_AFUNC_DEF((*cnn_transfer_derivative_list[])) = {
+	cnn_softmax_derivative,
+	cnn_relu_derivative,
+	cnn_swish_derivative
+};
+
+const char* cnn_transfer_func_name[] = {
+	"Softmax",
+	"ReLU",
+	"Swish"
+};
 
 CNN_AFUNC_DEF(cnn_softmax)
 {
 	int i;
-	float sum = 0;
+	float max, sum;
 
-	for(i = 0; i < len; i++)
+	// Find max value
+	max = src[0];
+	for(i = 1; i < len; i++)
 	{
-		sum += exp(src[i]);
+		if(src[i] > max)
+		{
+			max = src[i];
+		}
 	}
 
+	// Find exponential summation
+	sum = 0;
 	for(i = 0; i < len; i++)
 	{
-		dst[i] = exp(src[i]) / sum;
+		dst[i] = src[i] - max;
+		sum += exp(dst[i]);
 	}
+
+	// Find softmax output
+	for(i = 0; i < len; i++)
+	{
+		dst[i] = exp(dst[i]) / sum;
+	}
+}
+
+CNN_AFUNC_DEF(cnn_softmax_derivative)
+{
+
 }
 
 CNN_AFUNC_DEF(cnn_relu)
 {
 
 }
+
+CNN_AFUNC_DEF(cnn_relu_derivative)
+{
+
+}
+
+CNN_AFUNC_DEF(cnn_swish)
+{
+
+}
+
+CNN_AFUNC_DEF(cnn_swish_derivative)
+{
+
+}
+
--- src/cnn_builtin_math.h
+++ src/cnn_builtin_math.h
@@ -1,15 +1,26 @@
 #ifndef __CNN_BUILTIN_MATH_H__
 #define __CNN_BUILTIN_MATH_H__
 
+#define CNN_TFUNC_AMOUNT 3
+
 #define CNN_AFUNC_DEF(name) void name(float* dst, float* src, int len)
+
+extern CNN_AFUNC_DEF((*cnn_transfer_list[]));
+extern CNN_AFUNC_DEF((*cnn_transfer_derivative_list[]));
+extern const char* cnn_transfer_func_name[];
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 CNN_AFUNC_DEF(cnn_softmax);
+CNN_AFUNC_DEF(cnn_softmax_derivative);
 
 CNN_AFUNC_DEF(cnn_relu);
+CNN_AFUNC_DEF(cnn_relu_derivative);
+
+CNN_AFUNC_DEF(cnn_swish);
+CNN_AFUNC_DEF(cnn_swish_derivative);
 
 #ifdef __cplusplus
 }
