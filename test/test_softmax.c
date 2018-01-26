559a3f4f3f5134c9bdcc03c2163b27109c0cf994
Zheng-Ling Lai <jamesljlster@gmail.com>
2018-01-26 10:56:29 +0800

    test_softmax: WIP

--- test/test_softmax.c
+++ test/test_softmax.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
 #include <cnn_builtin_math.h>
+
+#define DX 0.00001
 
 void print_vec(float* src, int len);
 
 int main(int argc, char* argv[])
 {
-	int i;
+	int i, j;
 	int len;
 
 	float* src = NULL;
 	float* dst = NULL;
+	float* buf = NULL;
+	float* deri = NULL;
+	float* grad = NULL;
+
+	float err;
 
 	// Check argument
 	if(argc <= 1)
@@ -23,7 +32,10 @@ int main(int argc, char* argv[])
 	len = argc - 1;
 	src = calloc(len, sizeof(float));
 	dst = calloc(len, sizeof(float));
-	if(src == NULL || dst == NULL)
+	buf = calloc(len, sizeof(float));
+	deri = calloc(len * len, sizeof(float));
+	grad = calloc(len * len, sizeof(float));
+	if(src == NULL || dst == NULL || buf == NULL || deri == NULL || grad == NULL)
 	{
 		printf("Memory allocation failed!\n");
 		return -1;
@@ -36,12 +48,58 @@ int main(int argc, char* argv[])
 	}
 
 	// Run softmax
-	cnn_softmax(dst, src, len);
+	cnn_softmax(dst, src, len, NULL);
 
+	printf("Test softmax:\n");
 	printf("src: ");
 	print_vec(src, len);
 	printf("dst: ");
 	print_vec(dst, len);
+	printf("\n");
+
+	// Find grad
+	for(i = 0; i < len; i++)
+	{
+		for(j = 0; j < len; j++)
+		{
+			src[j] = atof(argv[j + 1]);
+			if(i == j)
+			{
+				src[j] += DX;
+			}
+		}
+
+		cnn_softmax(deri, src, len, NULL);
+		grad[i] = deri[i];
+	}
+
+	for(i = 0; i < len; i++)
+	{
+		grad[i] = (grad[i] - dst[i]) / DX;
+	}
+
+	// Find derivative
+	for(i = 0; i < len; i++)
+	{
+		src[i] = atof(argv[i + 1]);
+	}
+
+	cnn_softmax_grad(deri, src, len, buf);
+
+	// Find error
+	err = 0;
+	for(i = 0; i < len; i++)
+	{
+		err += fabs(grad[i] - deri[i]);
+	}
+
+	printf("Test softmax derivative:\n");
+	printf("deri: ");
+	print_vec(deri, len);
+	printf("grad: ");
+	print_vec(grad, len);
+	printf("Sum error: %lf\n", err);
+	printf("\n");
 
 	return 0;
 }
