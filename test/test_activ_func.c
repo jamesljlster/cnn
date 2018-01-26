ca1d0667cadb79b69a7fa948326a5399c73bca1a
Zheng-Ling Lai <jamesljlster@gmail.com>
2018-01-26 16:25:33 +0800

    Update test_activ_func.c

--- test/test_activ_func.c
+++ test/test_activ_func.c
@@ -1,8 +1,118 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <math.h>
 
 #include <cnn_builtin_math.h>
 
-int main()
+void print_mat(float* src, int rows, int cols);
+
+int main(int argc, char* argv[])
 {
+	int i, j;
+	int len;
+
+	float* src = NULL;
+	float* dst = NULL;
+	float* buf = NULL;
+	float* deri = NULL;
+	float* grad = NULL;
+
+	float err;
+	float dx = pow(10, -4);
+
+	// Check dx
+	assert(dx != 0.0f);
+
+	// Check argument
+	if(argc <= 1)
+	{
+		printf("Assign arguments with real numbers to run the program\n");
+		return -1;
+	}
+
+	// Memory allocation
+	len = argc - 1;
+	src = calloc(len, sizeof(float));
+	dst = calloc(len, sizeof(float));
+	buf = calloc(len, sizeof(float));
+	deri = calloc(len * len, sizeof(float));
+	grad = calloc(len * len, sizeof(float));
+	if(src == NULL || dst == NULL || buf == NULL || deri == NULL || grad == NULL)
+	{
+		printf("Memory allocation failed!\n");
+		return -1;
+	}
+
+	// Parse argument
+	for(i = 0; i < len; i++)
+	{
+		src[i] = atof(argv[i + 1]);
+	}
+
+	// Find grad
+	cnn_softmax(dst, src, len, NULL);
+	for(i = 0; i < len; i++)
+	{
+		for(j = 0; j < len; j++)
+		{
+			src[j] = atof(argv[j + 1]);
+			if(i == j)
+			{
+				src[j] += dx;
+			}
+		}
+
+		cnn_softmax(buf, src, len, NULL);
+
+		for(j = 0; j < len; j++)
+		{
+			grad[i * len + j] = (buf[j] - dst[j]) / dx;
+		}
+	}
+
+	// Find derivative
+	for(i = 0; i < len; i++)
+	{
+		src[i] = atof(argv[i + 1]);
+	}
+
+	cnn_softmax_grad(deri, src, len, buf);
+
+	// Find error
+	err = 0;
+	for(i = 0; i < len * len; i++)
+	{
+		err += fabs(grad[i] - deri[i]);
+	}
+
+	printf("Test softmax derivative:\n");
+	printf("deri:\n");
+	print_mat(deri, len, len);
+	printf("\n");
+	printf("grad:\n");
+	print_mat(grad, len, len);
+	printf("\n");
+	printf("Sum error: %lf\n", err);
+	printf("\n");
+
 	return 0;
+}
+
+void print_mat(float* src, int rows, int cols)
+{
+	int i, j;
+	for(i = 0; i < rows; i++)
+	{
+		printf(" | ");
+		for(j = 0; j < cols; j++)
+		{
+			printf("%+f", src[i * cols + j]);
+			if(j < cols - 1)
+			{
+				printf("  ");
+			}
+		}
+		printf(" |\n");
+	}
 }
