75bf73426e7cb32bdee26a8c16ac008d7001014b
Zheng-Ling Lai <jamesljlster@gmail.com>
2018-01-29 15:30:52 +0800

    Add test_config_create_stable

---
+++ test/test_config_create_stable.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#include <cnn.h>
+#include <cnn_types.h>
+
+int main()
+{
+	int ret;
+	cnn_config_t cfg = NULL;
+
+	while(1)
+	{
+		ret = cnn_config_create(&cfg);
+		if(ret < 0)
+		{
+			printf("cnn_config_create() failed with error: %d\n", ret);
+			return -1;
+		}
+
+		// Cleanup
+		cnn_config_delete(cfg);
+	}
+
+	return 0;
+}
