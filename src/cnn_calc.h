7b2e44a577cd5ba3a1b8870afa80b06b934aaf12
Zheng-Ling Lai <jamesljlster@gmail.com>
2018-01-30 15:54:22 +0800

    Add cnn_conv_2d() and test

--- src/cnn_calc.h
+++ src/cnn_calc.h
@@ -1,6 +1,22 @@
 #ifndef __CNN_CALC_H__
 #define __CNN_CALC_H__
 
-
+#define cnn_conv_2d(dst, dstRows, dstCols, kernel, kernelSize, src, srcRows, srcCols) \
+	for(int __row = 0; __row < dstRows; __row++) \
+	{ \
+		for(int __col = 0; __col < dstCols; __col++) \
+		{ \
+			float __conv = 0; \
+			for(int __convRow = 0; __convRow < kernelSize; __convRow++) \
+			{ \
+				for(int __convCol = 0; __convCol < kernelSize; __convCol++) \
+				{ \
+					__conv += kernel[__convRow * kernelSize + __convCol] * \
+						src[(__row + __convRow) * srcCols + (__col + __convCol)]; \
+				} \
+			} \
+			dst[__row * dstCols + __col] = __conv; \
+		} \
+	}
 
 #endif
