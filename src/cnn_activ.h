b7e840a7cbf5a53dd6d2457c32ea20b681788ab7
Zheng-Ling Lai <jamesljlster@gmail.com>
2019-06-21 00:16:43 +0800

    Performance improving in batch

--- src/cnn_activ.h
+++ src/cnn_activ.h
@@ -18,92 +18,140 @@
 static inline void cnn_forward_activ(union CNN_LAYER* layerRef,
                                      struct CNN_CONFIG* cfgRef, int layerIndex)
 {
-    for (int j = 0; j < cfgRef->batch; j++)
+    cnn_activ_t id = cfgRef->layerCfg[layerIndex].activ.id;
+
+    if (id > 0)
     {
-        int srcShift = j * layerRef[layerIndex - 1].outMat.data.cols;
-        int dstShift = j * layerRef[layerIndex].outMat.data.cols;
+        float* srcPtr = layerRef[layerIndex - 1].outMat.data.mat;
+        float* dstPtr = layerRef[layerIndex].outMat.data.mat;
+        float* bufPtr = layerRef[layerIndex].activ.buf.mat;
 
-        float* srcPtr = layerRef[layerIndex - 1].outMat.data.mat + srcShift;
-        float* dstPtr = layerRef[layerIndex].outMat.data.mat + dstShift;
-        float* bufPtr = layerRef[layerIndex].activ.buf.mat + dstShift;
+        cnn_activ_list[id](
+            dstPtr, srcPtr,
+            cfgRef->batch * layerRef[layerIndex].outMat.data.cols, bufPtr);
+    }
+    else
+    {
+        for (int j = 0; j < cfgRef->batch; j++)
+        {
+            int srcShift = j * layerRef[layerIndex - 1].outMat.data.cols;
+            int dstShift = j * layerRef[layerIndex].outMat.data.cols;
 
-        cnn_activ_list[cfgRef->layerCfg[layerIndex].activ.id](
-            dstPtr, srcPtr, layerRef[layerIndex].outMat.data.cols, bufPtr);
+            float* srcPtr = layerRef[layerIndex - 1].outMat.data.mat + srcShift;
+            float* dstPtr = layerRef[layerIndex].outMat.data.mat + dstShift;
+            float* bufPtr = layerRef[layerIndex].activ.buf.mat + dstShift;
+
+            cnn_activ_list[id](dstPtr, srcPtr,
+                               layerRef[layerIndex].outMat.data.cols, bufPtr);
+        }
     }
 }
 
 static inline void cnn_backward_activ(union CNN_LAYER* layerRef,
                                       struct CNN_CONFIG* cfgRef, int layerIndex)
 {
-    int srcShift, dstShift;
-    float* srcPtr;
-    float* dstPtr;
+    cnn_activ_t id = cfgRef->layerCfg[layerIndex].activ.id;
 
     if (layerIndex > 1)
     {
-        for (int j = 0; j < cfgRef->batch; j++)
+        if (id > 0)
         {
-            srcShift = j * layerRef[layerIndex - 1].outMat.data.cols;
-            if (cfgRef->layerCfg[layerIndex].activ.id == CNN_SOFTMAX)
-            {
-                dstShift = srcShift * layerRef[layerIndex].outMat.data.cols;
-            }
-            else
-            {
-                dstShift = srcShift;
-            }
+            float* srcPtr = layerRef[layerIndex - 1].outMat.data.mat;
+            float* dstPtr = layerRef[layerIndex].activ.gradMat.mat;
 
-            srcPtr = layerRef[layerIndex - 1].outMat.data.mat + srcShift;
-            dstPtr = layerRef[layerIndex].activ.gradMat.mat + dstShift;
+            float* gradIn = layerRef[layerIndex].outMat.data.grad;
+            float* gradOut = layerRef[layerIndex - 1].outMat.data.grad;
+
+            int size = cfgRef->batch * layerRef[layerIndex].outMat.data.cols;
 
             // Find gradient matrix
             cnn_activ_grad_list[cfgRef->layerCfg[layerIndex].activ.id](
-                dstPtr, srcPtr, layerRef[layerIndex].outMat.data.cols,
-                layerRef[layerIndex].outMat.data.mat + srcShift);
+                dstPtr, srcPtr, size, layerRef[layerIndex].outMat.data.mat);
 
             // Find layer gradient
-            if (cfgRef->layerCfg[layerIndex].activ.id == CNN_SOFTMAX)
-            {
 #ifdef CNN_WITH_CUDA
-                float alpha = 1.0;
-                float beta = 0.0;
-                cublasSgemm(
-                    cnnInit.blasHandle, CUBLAS_OP_N, CUBLAS_OP_N,
-                    layerRef[layerIndex].outMat.data.cols, 1,
-                    layerRef[layerIndex].outMat.data.cols, &alpha, dstPtr,
-                    layerRef[layerIndex].activ.gradMat.cols,
-                    layerRef[layerIndex].outMat.data.grad + srcShift,
-                    layerRef[layerIndex].outMat.data.cols, &beta,
-                    layerRef[layerIndex - 1].outMat.data.grad + srcShift,
-                    layerRef[layerIndex - 1].outMat.data.cols);
+            cnn_elemwise_product_gpu(gradOut, dstPtr, gradIn, size);
 #else
-                cblas_sgemm(
-                    CblasRowMajor, CblasNoTrans, CblasNoTrans, 1,
-                    layerRef[layerIndex - 1].outMat.data.cols,
-                    layerRef[layerIndex].outMat.data.cols, 1.0,
-                    &layerRef[layerIndex].outMat.data.grad[srcShift],
-                    layerRef[layerIndex].outMat.data.cols, dstPtr,
-                    layerRef[layerIndex].activ.gradMat.cols, 0.0,
-                    &layerRef[layerIndex - 1].outMat.data.grad[srcShift],
-                    layerRef[layerIndex - 1].outMat.data.cols);
-#endif
+            for (int k = 0; k < size; k++)
+            {
+                gradOut[k] = dstPtr[k] * gradIn[k];
             }
-            else
-            {
-#ifdef CNN_WITH_CUDA
-                cnn_elemwise_product_gpu(
-                    layerRef[layerIndex - 1].outMat.data.grad + srcShift,
-                    dstPtr, layerRef[layerIndex].outMat.data.grad + srcShift,
-                    layerRef[layerIndex - 1].outMat.data.cols);
-#else
-                for (int k = 0; k < layerRef[layerIndex - 1].outMat.data.cols;
-                     k++)
-                {
-                    layerRef[layerIndex - 1].outMat.data.grad[srcShift + k] =
-                        dstPtr[k] *
-                        layerRef[layerIndex].outMat.data.grad[srcShift + k];
-                }
 #endif
+        }
+        else
+        {
+            for (int j = 0; j < cfgRef->batch; j++)
+            {
+                int srcShift;
+                int dstShift;
+
+                float* srcPtr;
+                float* dstPtr;
+
+                srcShift = j * layerRef[layerIndex - 1].outMat.data.cols;
+                if (cfgRef->layerCfg[layerIndex].activ.id == CNN_SOFTMAX)
+                {
+                    dstShift = srcShift * layerRef[layerIndex].outMat.data.cols;
+                }
+                else
+                {
+                    dstShift = srcShift;
+                }
+
+                srcPtr = layerRef[layerIndex - 1].outMat.data.mat + srcShift;
+                dstPtr = layerRef[layerIndex].activ.gradMat.mat + dstShift;
+
+                // Find gradient matrix
+                cnn_activ_grad_list[cfgRef->layerCfg[layerIndex].activ.id](
+                    dstPtr, srcPtr, layerRef[layerIndex].outMat.data.cols,
+                    layerRef[layerIndex].outMat.data.mat + srcShift);
+
+                // Find layer gradient
+                if (cfgRef->layerCfg[layerIndex].activ.id == CNN_SOFTMAX)
+                {
+#ifdef CNN_WITH_CUDA
+                    float alpha = 1.0;
+                    float beta = 0.0;
+                    cublasSgemm(
+                        cnnInit.blasHandle, CUBLAS_OP_N, CUBLAS_OP_N,
+                        layerRef[layerIndex].outMat.data.cols, 1,
+                        layerRef[layerIndex].outMat.data.cols, &alpha, dstPtr,
+                        layerRef[layerIndex].activ.gradMat.cols,
+                        layerRef[layerIndex].outMat.data.grad + srcShift,
+                        layerRef[layerIndex].outMat.data.cols, &beta,
+                        layerRef[layerIndex - 1].outMat.data.grad + srcShift,
+                        layerRef[layerIndex - 1].outMat.data.cols);
+#else
+                    cblas_sgemm(
+                        CblasRowMajor, CblasNoTrans, CblasNoTrans, 1,
+                        layerRef[layerIndex - 1].outMat.data.cols,
+                        layerRef[layerIndex].outMat.data.cols, 1.0,
+                        &layerRef[layerIndex].outMat.data.grad[srcShift],
+                        layerRef[layerIndex].outMat.data.cols, dstPtr,
+                        layerRef[layerIndex].activ.gradMat.cols, 0.0,
+                        &layerRef[layerIndex - 1].outMat.data.grad[srcShift],
+                        layerRef[layerIndex - 1].outMat.data.cols);
+#endif
+                }
+                else
+                {
+#ifdef CNN_WITH_CUDA
+                    cnn_elemwise_product_gpu(
+                        layerRef[layerIndex - 1].outMat.data.grad + srcShift,
+                        dstPtr,
+                        layerRef[layerIndex].outMat.data.grad + srcShift,
+                        layerRef[layerIndex - 1].outMat.data.cols);
+#else
+                    for (int k = 0;
+                         k < layerRef[layerIndex - 1].outMat.data.cols; k++)
+                    {
+                        layerRef[layerIndex - 1]
+                            .outMat.data.grad[srcShift + k] =
+                            dstPtr[k] *
+                            layerRef[layerIndex].outMat.data.grad[srcShift + k];
+                    }
+#endif
+                }
             }
         }
     }
