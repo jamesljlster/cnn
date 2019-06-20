801d90fab51a9775ff0bf82d7276d79c2f6cc2b6
Zheng-Ling Lai <jamesljlster@gmail.com>
2019-06-20 17:28:23 +0800

    Find openmp package as depencency

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -56,6 +56,14 @@ include_directories(${DEPS_PATHS}
 #	include_directories(${Check_INCLUDE_DIR})
 #endif()
 
+# Find OpenMP
+find_package(OpenMP QUIET)
+if(${OpenMP_FOUND})
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${OpenMP_EXE_LINKER_FLAGS}")
+endif()
+
 # Add subdirectories
 foreach(DEPS_PATH ${DEPS_PATHS})
     add_subdirectory(${DEPS_PATH})
