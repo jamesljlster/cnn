e86763bb5e2e670b7c74c7b3e4de36bf5ec9a7bd
Zheng-Ling Lai <jamesljlster@gmail.com>
2019-09-11 18:32:04 +0800

    Add cudnn_log() for debugging

--- test/test_fc_perf.c
+++ test/test_fc_perf.c
@@ -60,6 +60,10 @@ int main(int argc, char* argv[])
                             (struct CNN_CONFIG_LAYER_FC*)&cfg->layerCfg[2],
                             sizeOut, 1, 1, cfg->batch));
 
+#ifdef CNN_WITH_CUDA
+    test(cnn_cudnn_ws_alloc());
+#endif
+
     // Performance test
     printf("Forward (ms), Backward (ms)\n");
     for (int s = 0; s < samples; s++)
